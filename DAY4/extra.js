d3bb4f8c7a08a80083db10213bdc09960ad58147
Nasar Haider Jafri <nasarjafri14@gmail.com>
2025-12-12 11:04:13 +0530

    day 5 work

---
+++ DAY4/extra.js
@@ -0,0 +1,6 @@
+let mydate = new Date();
+console.log(mydate.toLocaleString())
+console.log(mydate.toUTCString())
+console.log(mydate.toISOString())
+console.log(mydate.toJSON())
+console.log(mydate.getTimezoneOffset())
